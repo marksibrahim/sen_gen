f7b60002269bc9d72a84c62e268a650362805a6b
marksibrahim <mark.ibhm@gmail.com>
2016-03-01 18:03:46 -0500

    utf-8 encoding and file open on windows

--- story.py
+++ story.py
@@ -11,7 +11,7 @@ corpus_path = "clean/all_utf-8.txt"
 class Story():
 
     def __init__(self):
-        with open(corpus_path, 'r') as corp:
+        with open(corpus_path, encoding="utf-8") as corp:
             self.markov_generator = markov.Markov(corp)
 
         
@@ -21,7 +21,7 @@ class Story():
         generates a sentence based on short story corpus 
         requires markov.py
         """
-        raw_sen_text = self.markov_generator.generate_markov_text(size=100)
+        raw_sen_text = self.markov_generator.generate_markov_text(size=200)
         sentence_list = sent_tokenize(raw_sen_text)
         sentence_list.pop(0)
         sentence_list.pop()
